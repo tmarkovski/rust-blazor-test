4bfa41dda3187472f7a71544b339619b1fb58732
Tomislav Markovski <tomislav@trinsic.id>
2021-11-12 18:15:48 -0500

    Init

---
+++ rust/src/lib.rs
@@ -0,0 +1,14 @@
+
+#[no_mangle]
+extern "C" fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
